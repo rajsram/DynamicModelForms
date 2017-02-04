2b6c0c27ea8b34c2315e32c1e4cc44d5465038de
cogwave <cogwave@COG100>
2017-02-04 10:32:27 +0530

    project added

---
+++ DynamicModelForm/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using DynamicModelForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicModelForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new StudentModel());
+        }
+        [HttpPost]
+        public ActionResult Index(StudentModel stu)
+        {
+            //var subjects = form["Subject"].Split(',');
+            //var modelsubjects = new List<SubjectModel>();
+            //foreach (var subject in subjects)
+            //{
+            //    modelsubjects.Add(new SubjectModel() { Subject = subject });
+            //}
+
+            //var model = new StudentModel();
+            //model.Name = form["Name"];
+            //model.Grade = form["Grade"];
+            //model.Tutor = form["Tutor"];
+            //model.Subjects = modelsubjects;
+
+            //Here I get the model from a dynamic form
+            if (ModelState.IsValid)
+            {
+                //db operations
+                return RedirectToAction("Index");
+            }
+            return View(stu);
+        }
+        public ActionResult Subjects(int count)
+        {
+            ViewBag.count = count;
+            return PartialView("SubjectPV");
+        }
+    }
+}
---
+++ DynamicModelForm/Models/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DynamicModelForm.Models
+{
+    public class StudentModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Grade { get; set; }
+        public string Tutor { get; set; }
+        public List<SubjectModel> Subjects { get; set; }
+    }
+
+    public class SubjectModel
+    {
+        [Required]
+        public string Subject { get; set; }
+    }
+}
